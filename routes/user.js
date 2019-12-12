95fa4d7cb6170a7651a2a57f8e6d8fc9157d496b
SwapnadeepMohapatra <swapnadeep456@gmail.com>
2019-12-12 19:49:29 +0530

    reformated the code

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const mysql = require("mysql");
+const firebase = require("firebase");
+
+var firebaseConfig = {
+  apiKey: "AIzaSyATkGhtjsq4L-E311L3gqVJUNLSneru_MU",
+  authDomain: "gci-osgeo.firebaseapp.com",
+  databaseURL: "https://gci-osgeo.firebaseio.com",
+  projectId: "gci-osgeo",
+  storageBucket: "gci-osgeo.appspot.com",
+  messagingSenderId: "200236554519",
+  appId: "1:200236554519:web:83b7b9ee9e585cf2cbbf52"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 const pool = mysql.createPool({
   connectionLimit: 10,
@@ -9,12 +22,20 @@ const pool = mysql.createPool({
   password: "b07b2cef",
   database: "heroku_fc72d7c948e70b5"
 });
+// const pool = mysql.createPool({
+//   connectionLimit: 10,
+//   host: "localhost",
+//   user: "root",
+//   database: "osgeo_project"
+// });
 
 function getConnection() {
   return pool;
 }
 
 router.get("/projects", (req, res) => {
+  requestSent();
+
   const connection = getConnection();
   const queryString = "SELECT * FROM projects";
   connection.query(queryString, (err, rows, fields) => {
@@ -30,6 +51,8 @@ router.get("/projects", (req, res) => {
 router.get("/projects/:id", (req, res) => {
   console.log(req.params.id);
 
+  requestSent();
+
   const connection = getConnection();
 
   const projectsId = req.params.id;
@@ -42,24 +65,25 @@ router.get("/projects/:id", (req, res) => {
       return;
     }
 
-    console.log(rows);
+    // console.log(rows);
     res.json(rows);
   });
 });
 
 router.post("/project_create", (req, res) => {
   console.log("Trying to create a new projects...");
-
+  requestSent();
   const projectName = req.body.create_project_name;
   const projectDesc = req.body.create_project_desc;
   const projectImgLink = req.body.create_project_img_link;
   const projectLink = req.body.create_project_link;
+  const projectType = req.body.create_project_type;
 
   const queryString =
-    "INSERT INTO projects (project_name, project_desc, project_img_link, project_link) VALUES (?, ?, ?, ?)";
+    "INSERT INTO projects (name, tagLine, imageUrl, url, type) VALUES (?, ?, ?, ?, ?)";
   getConnection().query(
     queryString,
-    [projectName, projectDesc, projectImgLink, projectLink],
+    [projectName, projectDesc, projectImgLink, projectLink, projectType],
     (err, results, fields) => {
       if (err) {
         console.log("Failed to insert new project: " + err);
@@ -72,5 +96,15 @@ router.post("/project_create", (req, res) => {
     }
   );
 });
+
+function requestSent() {
+  console.log("Request Sent");
+
+  firebase
+    .database()
+    .ref()
+    .child("stats")
+    .push("now");
+}
 
 module.exports = router;
